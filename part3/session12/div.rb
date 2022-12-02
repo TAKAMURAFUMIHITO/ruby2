14e623e46b0d55342a04d27038d62412f545763c
EC2 Default User <fumipro0913@gmail.com>
2022-12-02 14:03:02 +0000

    割り算,数値型の変換

---
+++ part3/session12/div.rb
@@ -0,0 +1,23 @@
+# div 割り算の商（整数）
+p 5.div(2)
+p 5.div(2.2)
+p -5.div(2)
+p -5.div(2.2)
+
+# quo 割り算の商
+p 5.quo(2)
+p 5.quo(2.2)
+p -5.quo(2)
+p -5.quo(2.2)
+
+# modulo 割り算の余り
+p 5.modulo(2)
+p 5.modulo(2.2)
+p -5.modulo(2)
+p -5.modulo(2.2)
+
+# divmod 割り算の商と余りを配列で表示
+p 10.divmod(3.5)
+p 10.divmod(-3.5)
+p -10.divmod(3.5)
+p -10.divmod(-3.5)
---
+++ part3/session12/round.rb
@@ -0,0 +1,12 @@
+# 四捨五入
+p 0.12.round(1)
+p 0.18.round(1)
+p 1.2.round
+p 1.8.round
+p 120.round(-2)
+p 180.round(-2)
+
+p 1.5.ceil
+p -1.5.ceil
+p 1.5.floor
+p -1.5.fllor
